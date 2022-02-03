b7632f4e09536339b5e411a128b338b02709af20
Lalit Bagga <davin12x@gmail.com>
2022-02-03 07:51:42 -0500

    assosicae type added

---
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedLoader.swift
@@ -0,0 +1,19 @@
+//
+//  Copyright © 2018 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+public enum LoadFeedResult<Error: Swift.Error> {
+	case success([FeedItem])
+	case failure(Error)
+}
+
+extension LoadFeedResult: Equatable where Error: Equatable {
+    
+}
+
+protocol FeedLoader {
+    associatedtype Error: Swift.Error
+    func load(completion: @escaping (LoadFeedResult<Error>) -> Void)
+}
